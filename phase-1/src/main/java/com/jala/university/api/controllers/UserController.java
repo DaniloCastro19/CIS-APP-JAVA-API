afbbe3a3d1b13f6c9312d5f3052e6f0935ec277e
Dan_19 <Danilo.Castro@jala.university>
2024-09-04 20:07:14 -0500

    feat:15 - Implementing POST method to Users API

--- phase-1/src/main/java/com/jala/university/api/controllers/UserController.java
+++ phase-1/src/main/java/com/jala/university/api/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.jala.university.api.controllers;
 
+import com.jala.university.core.models.UserModel;
+import com.jala.university.core.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping()
+    public UserModel createUser(@RequestBody UserModel user){
+        return userService.createUser(user);
+    }
 }
--- phase-1/src/main/java/com/jala/university/core/models/UserModel.java
+++ phase-1/src/main/java/com/jala/university/core/models/UserModel.java
@@ -1,4 +1,57 @@
 package com.jala.university.core.models;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false, length = 36)
+    private String id;
+
+    @NotEmpty(message = "Name Can't be blank")
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @NotEmpty(message = "Login Can't be blank")
+    @Column(nullable = false, length = 20)
+    private String login;
+
+    @NotEmpty(message = "Password Can't be blank")
+    @Column(nullable = false, length = 100)
+    private String password;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
