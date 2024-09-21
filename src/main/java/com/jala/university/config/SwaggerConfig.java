0f0f2cdede8230dc9ac080e424a43ce5a4900bc9
ccmc_24 <cristian.manrique@jala.university>
2024-09-20 20:49:54 -0500

    feat: implement-swagger

---
+++ src/main/java/com/jala/university/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.jala.university.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springdoc.core.models.GroupedOpenApi;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("spring-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
