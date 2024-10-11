e39799815138f0c08c0e460a77da9ccda4b837f7
Dan_19 <danilo.castro@jala.university>
2024-10-10 20:56:11 -0500

    feat: Turning gets to public

--- src/main/java/com/jala/university/core/security/SecurityConfiguration.java
+++ src/main/java/com/jala/university/core/security/SecurityConfiguration.java
@@ -18,7 +18,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users/validate").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/users/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/**").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/api/users/**").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/api/users/**").authenticated()
