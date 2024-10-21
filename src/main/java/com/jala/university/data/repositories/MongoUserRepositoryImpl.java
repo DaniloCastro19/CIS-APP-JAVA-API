4f71c2e89da04338719f9f46d9ca7c0a1a1bbd7f
sofiporras <sofia.porras@jala.university>
2024-10-21 15:41:29 -0500

    feat: Implement UUID as ID for Users and configure the MongoDB collection with the name users

--- src/main/java/com/jala/university/data/repositories/MongoUserRepositoryImpl.java
+++ src/main/java/com/jala/university/data/repositories/MongoUserRepositoryImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 @ConditionalOnProperty(name= "database.type", havingValue = "mongo")
@@ -20,6 +21,7 @@ public class MongoUserRepositoryImpl implements UserRepository{
 
     @Override
     public UserModel createUser(UserModel user) {
+        user.setId(UUID.randomUUID().toString());
         return mongoTemplate.save(user);
     }
 
